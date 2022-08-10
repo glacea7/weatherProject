029a7962723c9a3419cf09f35f8aab6025e64558
glacea7 <62964963+glacea7@users.noreply.github.com>
2022-08-10 15:10:34 -0400

    Updates

---
+++ precache-manifest.42783239dace12ff4a05430f6c414f5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2b273f9efc0e5d04f948338111995f",
+    "url": "/weatherProject/index.html"
+  },
+  {
+    "revision": "d100bf176cf95cb8289a",
+    "url": "/weatherProject/static/css/main.b4402bae.chunk.css"
+  },
+  {
+    "revision": "111a311936fe9bae948a",
+    "url": "/weatherProject/static/js/2.57a85075.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weatherProject/static/js/2.57a85075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d100bf176cf95cb8289a",
+    "url": "/weatherProject/static/js/main.4d197715.chunk.js"
+  },
+  {
+    "revision": "7f237b416398f66fdbda",
+    "url": "/weatherProject/static/js/runtime-main.96e8c700.js"
+  },
+  {
+    "revision": "84f3e25eb05185806dcd0271789bc832",
+    "url": "/weatherProject/static/media/thunderStorm.84f3e25e.jpeg"
+  }
+]);
